c4c736a26684338daebe19481f94b58977f78315
Gustavo Farias <gugafarias2009@gmail.com>
2023-05-22 18:36:52 -0300

    feat: add image upload and integrate new memory form with api

--- app/new.tsx
+++ app/new.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'expo-router'
+import { useState } from 'react'
+import { Link, useRouter } from 'expo-router'
 import {
   ScrollView,
   Switch,
@@ -6,16 +7,117 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  ImageBackground,
+  Alert,
 } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Icon from '@expo/vector-icons/Feather'
+import {
+  MediaTypeOptions,
+  PermissionStatus,
+  launchImageLibraryAsync,
+  useCameraPermissions,
+} from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { useState } from 'react'
+import { api } from '../src/assets/lib/api'
+
+interface MediaPreview {
+  mediaType: string
+  previewUri: string
+}
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const [cameraPermission, requestPermission] = useCameraPermissions()
+  const router = useRouter()
+
+  const [preview, setPreview] = useState<MediaPreview | null>(null)
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+
+  async function verifyPermission() {
+    if (cameraPermission.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission()
+
+      return permissionResponse.granted
+    }
+
+    if (cameraPermission.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Não foi possível acessar a câmera.',
+        'Para adicionar uma foto ou vídeo de capa você precisa permitir o acesso a câmera nas configuações do app.',
+      )
+
+      return false
+    }
+
+    return true
+  }
+
+  async function openImagePicker() {
+    try {
+      const hasCameraPermission = await verifyPermission()
+
+      if (!hasCameraPermission) {
+        return
+      }
+
+      const result = await launchImageLibraryAsync({
+        mediaTypes: MediaTypeOptions.All,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        const { type, uri } = result.assets[0]
+
+        setPreview({ mediaType: type, previewUri: uri })
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview.previewUri,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+    }
+
+    await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
@@ -52,27 +154,49 @@ export default function NewMemory() {
         </View>
 
         <TouchableOpacity
+          onPress={openImagePicker}
           activeOpacity={0.7}
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+          {preview ? (
+            // Implement video preview using react-native-video
+            <ImageBackground
+              source={{ uri: preview.previewUri }}
+              className="h-full w-full rounded-lg object-cover"
+              imageStyle={{ borderRadius: 5 }}
+            >
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => setPreview(null)}
+                className="absolute right-[-10] top-[-20] rounded-full bg-gray-900 px-3 py-3"
+              >
+                <Icon name="trash" size={16} color="#b54129" />
+              </TouchableOpacity>
+            </ImageBackground>
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
+          textAlignVertical="top"
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
 
         <TouchableOpacity
+          onPress={handleCreateMemory}
           activeOpacity={0.7}
-          className="items-center  self-end rounded-full bg-green-500 px-5 py-2"
+          className="items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
         </TouchableOpacity>
